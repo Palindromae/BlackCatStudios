0d68e3a4f9f13ad77643aa7b6b9f10bd905d0721
Felix <astronaut2022@gmail.com>
2022-11-24 16:00:17 +0000

    Some basic steps

--- core/src/com/mygdx/game/BlackCore/ItemAbs.java
+++ core/src/com/mygdx/game/BlackCore/ItemAbs.java
@@ -5,3 +5,16 @@ import com.mygdx.game.CoreData.Items.Items;
 public abstract class ItemAbs {
     public Items name;
 }
+
+
+/*
+
+Dictionary<Enum Recipe, recipe obj>
+list {
+
+    step{ done 25}
+    step{flip interaction
+    step[]
+
+}*/
+
