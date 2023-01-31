00b76a619583b622f57347210089f83756a32a03
Sam Toner <samueltoner@hotmail.co.uk>
2023-01-31 18:53:40 +0000

    Commented

--- core/src/com/mygdx/game/BlackScripts/CustomerManager.java
+++ core/src/com/mygdx/game/BlackScripts/CustomerManager.java
@@ -356,11 +356,11 @@ enum RandomisationStyle{
                 LeavingCustomers.remove(i);
         }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.S)){
+        /*if (Gdx.input.isKeyJustPressed(Input.Keys.S)){
             if(WaitingCustomers.size()>0)
             WaitingCustomers.get(0).RemoveFirstCustomer();
 
-        }
+        }*/
     }
         public void CreateMenu(){
             Menu = new LinkedList<>();
